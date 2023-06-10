d634bb397b9e719f4ebaf6f7534f1afc358180bd
judy-chelangat <judysigilai75@gmail.com>
2023-06-10 15:24:42 +0300

    updated completed assignment

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 function shout(string){
-    return string.toUpperCase();
+    return string.toUpperCase()
 }
 function whisper(string){
-    return string.toLowerCase();
+    return string.toLowerCase()
 }
- function logShout(string) {
-    console.log(string.toUpperCase());
- }
-function logWhisper(string)
+function logShout(string)
 {
-    console.log(string.toLowerCase());
+    console.log(string.toUpperCase())
+}
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+function sayHiToGrandma(){
+
 }
 function sayHiToHeadphonedRoommate(string){
-    if ( string === string.toLowerCase())
-    {
+    if (string === string.toLowerCase()){
         return "I can't hear you!"
     }
-    else if (string === string.toUpperCase())
-    {
+    else if (string===string.toUpperCase()){
         return "YES INDEED!"
     }
-    else if (string = "Let's have dinner together!")
-    {
+    else if (string === "Let's have dinner together!"){
         return "I would love to!"
     }
-    
-    
 }
-
-
-
-
-
-
